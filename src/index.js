a8ab6dbf127a0d70503ff2c5325c5f647791ba99
Artanty <thomasway7@gmail.com>
2020-09-30 21:31:12 +0300

    fetch1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FetchComponent from './FetchComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FetchComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
