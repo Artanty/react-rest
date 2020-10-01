80898566a8ee5ce2b05d5d550d200afbd5fb5c54
Artanty <thomasway7@gmail.com>
2020-10-01 21:01:24 +0300

    todo app on redux is working

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+
 import FetchComponent from './components/step3/FetchComponent';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <FetchComponent />
-  </React.StrictMode>,
+const store = createStore(rootReducer)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <FetchComponent />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
